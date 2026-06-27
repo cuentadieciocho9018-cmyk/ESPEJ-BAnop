d89178b701bd57c8b20b4d9bffdc848ab76ed8c2
cuentadieciocho9018-cmyk <cuentadieciocho9018@gmail.com>
2026-06-26 19:08:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,11 +22,11 @@ $v = time();
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1519920833259258');
+  fbq('init', '2642202329527626');
   fbq('track', 'PageView');
   </script>
   <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1519920833259258&ev=PageView&noscript=1"/></noscript>
+  src="https://www.facebook.com/tr?id=2642202329527626&ev=PageView&noscript=1"/></noscript>
   <!-- End Meta Pixel Code -->
 
   <!-- Modern stack signals (Tailwind + Google Fonts + Material Symbols) -->
